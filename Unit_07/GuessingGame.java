dbb8b6376343d273f979b3250085319dab0df21b
Jianing Wang <79324726+24wangj@users.noreply.github.com>
2021-10-05 09:44:59 -0400

    Update GuessingGame.java

--- Unit_07/GuessingGame.java
+++ Unit_07/GuessingGame.java
@@ -6,15 +6,15 @@ public class GuessingGame {
 	public static void main(String[] args) {
 		
 		Scanner keyboard = new Scanner(System.in);
-		int random = (int)(Math.random() * 100);
+		final int RANDOM = (int)(Math.random() * 1000);
 		
 		System.out.println("Guess a number:");
-		``
+		
 		for (int i = 10; i >= 0; i--) {
 			int guess = keyboard.nextInt();
-			if (guess < random) {
+			if (guess < RANDOM) {
 			    System.out.println("Too low! " + i + " guesses left.");
-			} else if (guess > random) {
+			} else if (guess > RANDOM) {
 			    System.out.println("Too high! " + i + " guesses left.");
 			} else {
 			    System.out.println("Correct!");
