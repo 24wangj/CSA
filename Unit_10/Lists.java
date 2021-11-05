2b280712d9835457f9599a9a582d90748421c4e9
Jianing Wang <79324726+24wangj@users.noreply.github.com>
2021-11-05 17:54:50 -0400

    Update Lists.java

--- Unit_10/Lists.java
+++ Unit_10/Lists.java
@@ -1,1 +1,34 @@
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Lists {
+
+	public static ArrayList<String> getNames() {
+		
+		Scanner keyboard = new Scanner(System.in);
+		ArrayList<String> names = new ArrayList<String>();
+		
+		System.out.println("How many names would you like to enter?");
+		int length = keyboard.nextInt();
+		
+		for (int i = 1; i <= length; i++) {
+			names.add(keyboard.nextLine());
+			System.out.println("Enter name (" + i + "):");
+		}
+		names.add(keyboard.nextLine());
+		keyboard.close();
+		
+		return names;
+	}
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> names = getNames();
+		
+		for (String i : names) {
+			System.out.println(i);
+		}
+	}
+}
